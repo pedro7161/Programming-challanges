aa8b24b252f108ad2f150b2c9f93df9776164b52
pedro pinto <pedro.ramos9@hotmail.com>
2021-12-16 12:59:37 +0000

    fizzbuzz finishe

--- FizzBuzz/main.py
+++ FizzBuzz/main.py
@@ -1,9 +1,11 @@
 # 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz
-i=100
+i=100000000000
 for k in range(0,i):
     if k%3==0 and k%5==1:
-        print("fizz" + k)
-    if k%5==0 and k%3==1:
-        print("Buzz" + k)
-    if k%5==0 and k%3==0:
-        print("FizzBuzz" + k)
+        print("fizz" )
+    elif k%5==0 and k%3==1:
+        print("Buzz" )
+    elif k%5==0 and k%3==0:
+        print("FizzBuzz")
+    else:
+        print(k)
